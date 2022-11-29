93e70b612b18b0c590cd1e2170c292f4b2aef4bc
Talgat The Random <36820511+RND332@users.noreply.github.com>
2022-11-30 03:23:34 +0500

    BRIP: add indicator

--- src/indicators.py
+++ src/indicators.py
@@ -1,14 +1,13 @@
-import os
-import sys
+"""
+Indicator is a class that is used to save the indicator data to file.
+"""
+
 import json
+import time
 import zlib
 import base64
-
-# JSON: 
-# "$graph-name": {
-#   "value": "$point x&y",
-#   "time": "$time-UNIX",
-# }
+from point import Point
+from pair import Pair
 
 class Indicator:
     """This is class that represents a any Indicator that is a point on a graph
@@ -18,32 +17,37 @@ class Indicator:
         value (int): The value of the indicator
         time (int): The UNIX time of the indicator
     """
-    name = ""
-    value = 0
-    time = 0
-    def __init__(self, name):
-        """Make a new indicator with the name of the indicator"""
-        self.name = name
-        self.value = 0
-        self.time = 0
+    name = ""               # name of the indicator
+    pair: Pair = None       # pair of the indicator
+    value: Point = None     # value of the indicator
+    time_unix = 0           # time of the indicator
     
-    def __init__(self, name, value, time):
+    def __init__(self, name, pair=Pair("BTC", "USDT"), value=Point(0, 0), time_unix=0, save=False):
+        """
+        Create a new indicator with the given name, value, and time.
+
+        Parameters
+        ----------
+        name : str
+            The name of the indicator
+        value : int
+            The value of the indicator
+        time : int
+            The UNIX time of the indicator
+        """
         self.name = name
+        self.pair = pair
         self.value = value
-        self.time = time
+        self.time_unix = time_unix
 
-    def __init__(self, name, value, time) -> None:
-        self.name = name
-        self.value = value
-        self.time = time
+        if save:
+            self.write()
 
-        # write this indicator to the file as a JSON object and compress it via zlib, then convert it to base64
-        # if the file doesn't exist, create it
-        # if the file does exist, append to it
 
-        self.write()
-
-    def write(self):
+    def write(self, file_name=name + str(int(time.time())) + ".indicator"):
+        """
+        Write the indicator to a file, with compression via zlib and encoding with base64.
+        """
         # make indicator into a serialable object
         object_dict = { Indicator: self }
         # convert to JSON string
@@ -57,7 +61,10 @@ class Indicator:
         # write to file
         # if the file doesn't exist, create it
         # if the file does exist, append to it
-        with open(self.name, "a") as file:
+
+        # file_name is $ticker+$UNIXtime_unix.indicator
+
+        with open(file_name, "a") as file:
             file.write(base64_object)
             file.write('\n')
 
@@ -65,12 +72,12 @@ class Indicator:
     def set_value(self, value):
         self.value = value
 
-    def set_time(self, time):
-        self.time = time
+    def set_time_unix(self, time_unix):
+        self.time_unix = time_unix
 
-    def set_indicator(self, value, time):
+    def set_indicator(self, value, time_unix):
         self.value = value
-        self.time = time
+        self.time_unix = time_unix
     
     def get_name(self):
         return self.name
@@ -78,16 +85,17 @@ class Indicator:
     def get_value(self):
         return self.value
 
-    def get_time(self):
-        return self.time
+    def get_time_unix(self):
+        return self.time_unix
 
     
 
 # test
 indicator = Indicator("test", 1, 2)
 indicator.set_value(3)
-indicator.set_time(4)
+indicator.set_time_unix(4)
 indicator.set_indicator(5, 6)
+indicator.write()
 print(indicator.get_name())
 print(indicator.get_value())
-print(indicator.get_time())
+print(indicator.get_time_unix())
---
+++ src/types/coin.py
@@ -0,0 +1,27 @@
+class Coin:
+    """
+    A class to represent a coin with tiker and name.
+
+    Attributes
+    ----------
+    ticker : str
+        The ticker of the coin
+    name : str
+        The name of the coin
+    """
+    ticker = ""
+    name = ""
+    def __init__(self, ticker, name):
+        """
+        Create a new coin with the given ticker and name.
+
+        Parameters
+        ----------
+        ticker : str
+            The ticker of the coin
+        name : str
+            The name of the coin
+        """
+        self.ticker = ticker
+        self.name = name
+
